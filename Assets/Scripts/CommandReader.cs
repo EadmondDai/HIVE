b641b56ed6a3449e7a02e73285dc0291212bd589
archer-yang <xyang42@ucsc.edu>
2016-10-22 19:54:05 -0700

    Initial Submit
    Oct 16 version

---
+++ Assets/Scripts/CommandReader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandReader : MonoBehaviour
+{
+    public TextMesh outText;
+    public string outputText = "";
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void input(string rawInput)
+    {
+        if (outputText == "")
+            outputText = rawInput;
+        else
+        {
+            //priorText;
+            outputText = outputText.Insert(outputText.Length, "\n\n" + rawInput);
+        }
+        outText.text = outputText;
+
+        if (rawInput == "help")
+        {
+            //outputText += "HOGS";
+
+            outputText = outputText.Insert(outputText.Length, "\n\nCMDS:\n"+ "dir\n" + "datajack\n" + "light");
+            outText.text = outputText;
+        }
+
+        else if (rawInput == "halp")
+        {
+            outputText = outputText.Insert(outputText.Length, "\n\nHALP");
+            outText.text = outputText;
+        }
+
+        else
+        {
+            outputText = outputText.Insert(outputText.Length, "\nCommand not recognized.");
+            outText.text = outputText;
+        }
+
+    }
+}
